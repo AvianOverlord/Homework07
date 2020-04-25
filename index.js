48aa77dc19dfb1a7d846e82983a1bcae657fb2ee
William Rave <wrave@gwu.edu>
2020-04-25 16:22:17 -0400

    Wrote non-Git code.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const questions = [
         type: "checkbox",
         name: "sections",
         message: "Which sections should be included in this readme?",
-        choices: ["Installation","Usage","Contributing","Tests","Questions"];
+        choices: ["Installation","Usage","Contributing","Tests","Questions"]
     }
 ];
 
@@ -43,16 +43,85 @@ const optionalQuestions = [
         type: "input",
         name: "contributing",
         message: "Enter contributing instructions:"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter your tests:"
+    },
+    {
+        type: "inqut",
+        name: "questions",
+        message: "Enter your questions:"
     }
 ];
 // -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data);
 }
 
 // -- This is a fairly common programming construct. They are just giving us a FUNCTION to INITIALIZE or SETUP our project parameter. It's also where we usually kick off our project flow -- //
 function init() {
     inquirer.prompt(questions).then(data => {
+        const projectTitle = data.title;
+        const projectDesc = data.description;
+        const projectLic = data.license;
+        const options = data.sections;
 
+        //Code for asking the optional questions
+        const extraQuestions = [];
+        optionalQuestions.map(question => {
+            const questionName = question.name.replace(question.name.charAt(0),questions.name.toUpperCase().charAt(0));
+            if(options.findIndex(questionName) !== -1)
+            {
+                extraQuestions.push(questionName)
+            }
+
+        });
+        inquirer.prompt(extraQuestions).then(extraData => {
+            //Title is on top
+            let docText = "";
+            docText += `${projectTitle}\r\n`;
+
+            //Followed by the TOC
+            docText += `Table of Contents: \r\n -Description \r\n`;
+            options.map(option =>{
+                docText += `-${option}\r\n`;
+            });
+            docText += `-License \r\n`;
+            docText += `\r\n`;
+
+            //And a description
+            docText += `Description: ${projectDesc}\r\n`;
+
+            if(options.includes("Installation"))
+            {
+                docText += `Installation instructions: \r\n ${extraData.installation}\r\n`;
+            };
+
+            if(options.includes("Usage"))
+            {
+                docText += `Usage instructions: \r\n ${extraData.usage}\r\n`;
+            }
+
+            if(options.includes("Contributing"))
+            {
+                docText += `Contribution instructions: \r\n ${extraData.contributing} \r\n`;
+            }
+
+            if(options.includes("Tests"))
+            {
+                docText += `Tests: \r\n ${extraData.tests} \r\n`;
+            }
+
+            if(options.includes("Questions"))
+            {
+                docText += `Questions: \r\n ${questions} \r\n`;
+            }
+
+            docText += `License: \r\n ${projectLic}\r\n`;
+            writeToFile(`${projectTitle}.doc`,docText);
+        })
     });
 }
 
